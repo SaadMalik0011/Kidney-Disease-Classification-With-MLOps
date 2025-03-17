6d6544a5e275daefba7c277ce39d46e39d9aaf92
SaadMalik0011 <xs2saadmalik@gmail.com>
2025-03-18 03:58:18 +0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A deep learning project for kidney cancer detection using Convolutional Neural Networks (CNN). It includes a complete MLOps pipeline for automated data processing, model training, deployment, and monitoring.',
+    author='Saad Malik',
+    license='MIT',
+)
